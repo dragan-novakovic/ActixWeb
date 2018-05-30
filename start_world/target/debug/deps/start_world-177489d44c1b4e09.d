c1e5625a8b90daf8553ecec569e91c6890c8ad02
Dragan1810 <d.novakovic1810@yahoo.com>
2018-05-30 16:34:35 +0200

    qqq

---
+++ start_world/target/debug/deps/start_world-177489d44c1b4e09.d
@@ -0,0 +1,5 @@
+/home/dragan1810/Desktop/Hello-Rust/start_world/target/debug/deps/start_world-177489d44c1b4e09: src/main.rs
+
+/home/dragan1810/Desktop/Hello-Rust/start_world/target/debug/deps/start_world-177489d44c1b4e09.d: src/main.rs
+
+src/main.rs:
---
+++ start_world/target/debug/start_world.d
@@ -0,0 +1,1 @@
+/home/dragan1810/Desktop/Hello-Rust/start_world/target/debug/start_world: /home/dragan1810/Desktop/Hello-Rust/start_world/src/main.rs
