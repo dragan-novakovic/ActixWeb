5c3c68f5346724e6f359e54a72d1186f6f42c3ae
Dragan1810 <d.novakovic1810@yahoo.com>
2018-05-30 19:57:20 +0200

    qqq

--- start_world/src/main.rs
+++ start_world/src/main.rs
@@ -1,3 +1,34 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Object {
+    width: u32,
+    height: u32,
+}
+impl Object {
+    fn area(&self) -> u32 {
+        return self.height * self.width;
+    }
+
+    fn new(width: u32, height: u32) -> Object {
+        Object { width, height }
+    }
+
+    fn show(&self) {
+        println!("{}x{} with area: {}", self.width, self.height, self.area());
+    }
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.width, self.height)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let o = Object::new(50, 60);
+    let k = Object::new(50, 50);
+
+    o.show();
+    k.show();
 }
