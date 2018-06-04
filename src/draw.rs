44bd4382fe861ef263cf965f04c7c5fe7a575a54
Dragan1810 <d.novakovic1810@yahoo.com>
2018-06-04 19:49:14 +0200

    finished,  treba redo

--- src/draw.rs
+++ src/draw.rs
@@ -7,6 +7,10 @@ pub fn to_coords(game_coords: i32) -> f64 {
    (game_coords as f64) * BLOCK_SIZE
 }
 
+pub fn to_coords_u32(game_coords: i32) -> u32 {
+   to_coords(game_coords) as u32
+}
+
 pub fn draw_block(color: Color, x:i32, y:i32, con:&Context,g:&mut G2d) {
     let gui_x = to_coords(x);
     let gui_y = to_coords(y);
