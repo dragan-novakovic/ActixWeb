edc2812179cd1e4e2b4dcd593bc49c9aa7641aa9
Dragan <d.novakovic1810@yahoo.com>
2018-07-14 12:45:28 +0200

    HashMapzz

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 10);
 
+    let teams = vec![String::from("Blue"), String::from("Red")];
+    let initial_scores = vec![10, 50];
+
+    let new_scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
     let mut v = vec![10, 20, 30];
 
     for i in &mut v {
         *i = *i + 50;
     }
-    println!("Hello, world!, {:?}", &v);
+    println!("Hello, world!, {:?}", &new_scores);
 }
