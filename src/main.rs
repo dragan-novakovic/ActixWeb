f1c8bbe7721fe0be1d7d05fca7bc342ddc312800
Dragan <d.novakovic1810@yahoo.com>
2018-07-14 12:11:48 +0200

    hashMaps

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::collections::HashMap;
+
 fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 10);
+
     let mut v = vec![10, 20, 30];
 
     for i in &mut v {
